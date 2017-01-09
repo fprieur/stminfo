3ac94d5effef789022e1d5b41cd2460ae9fda4fc
Fred Prieur <fprieur@gmail.com>
2017-01-09 17:06:36 -0500

    init

---
+++ stminfo.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"strings"
+	"github.com/PuerkitoBio/goquery"
+	"fmt"
+)
+
+
+func ExampleScrape() {
+	doc, err := goquery.NewDocument("http://www.stm.info/fr/infos/etat-du-service/metro")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("div.content-block section.item").Each(func(i int, s *goquery.Selection) {
+		couleur := s.Find("h2").Text()
+		couleur = strings.Trim(couleur, " ")
+		couleur = strings.Replace(couleur, "\n", "", -1)
+		etat := s.Find("p").Text()
+		etat = strings.Trim(etat, " ")
+		etat = strings.Replace(etat, "\n", "", -1)
+		fmt.Printf("%s - %s\n", couleur, etat)
+	})
+}
+
+func main() {
+	ExampleScrape()
+}
